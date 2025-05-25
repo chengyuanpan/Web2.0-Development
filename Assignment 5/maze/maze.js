e032a438663b868adb850bbc9f203436ed72482c
Pan Chengyuan <panchy7@mail2.sysu.edu.cn>
2025-05-25 17:51:57 +0800

    Format code style

--- Assignment 5/maze/maze.js
+++ Assignment 5/maze/maze.js
@@ -17,13 +17,13 @@ function addListener() {
 	var i, j;
 	var _area, _path;
 	for (i = 1; i <= 6; i++) {
-		_area = document.getElementsByClassName("area"+i);
+		_area = document.getElementsByClassName("area" + i);
 		for (j = 0; j < _area.length; j++) {
 			_area[j].addEventListener('mouseover', warning);
 			_area[j].addEventListener('mouseout', myReset);
 		}
 		if (i <= 3) {
-			_path = document.getElementsByClassName("path"+i);
+			_path = document.getElementsByClassName("path" + i);
 			for (j = 0; j < _path.length; j++) _path[j].addEventListener('mouseover', changeMouse);
 		}
 	}
@@ -33,15 +33,15 @@ function changeMouse(event) {
 	if (start == 1) event.target.style.cursor = "pointer";
 }
 
-window.onload = function() {
+window.onload = function () {
 	addListener();
-	document.getElementById("s").onmouseover = function() {
+	document.getElementById("s").onmouseover = function () {
 		if (start == 0) {
 			start = 1;
 			document.getElementById("result").textContent = 'Gaming';
 		}
 	}
-	document.getElementById("e").onmouseover = function() {
+	document.getElementById("e").onmouseover = function () {
 		if (start == 1 && through == 1) {
 			start = 0;
 			through = 0;
@@ -52,7 +52,7 @@ window.onload = function() {
 			document.getElementById("result").textContent = 'Don\'t cheat!';
 		}
 	}
-	document.getElementsByClassName("path3")[0].onmouseover = function() {
+	document.getElementsByClassName("path3")[0].onmouseover = function () {
 		if (start == 1) {
 			if (through == 0) through = 1;
 		}
