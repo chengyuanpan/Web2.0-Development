de084fbc291976671bb15c0ea81b8ea7030d9bc5
Pan Chengyuan <panchy7@mail2.sysu.edu.cn>
2025-05-25 19:23:25 +0800

    Improve JS code

--- Assignment 5/maze/maze.js
+++ Assignment 5/maze/maze.js
@@ -1,60 +1,67 @@
-let start = 0;
-let through = 0;
+let isStart = false;
+let isThrough = false;
+const wallNumber = 6;
+const pathNumber = 3;
 
 function warning(event) {
-	if (start == 1) {
-		start = 0;
+	if (isStart) {
+		isStart = false;
 		event.target.id = 'error';
 		document.getElementById("result").textContent = 'You lose';
 	}
 }
 
-function myReset(event) {
-	if (event.target.id == 'error') event.target.id = 'wall';
-}
-
-function addListener() {
-	let i, j;
-	let _area, _path;
-	for (i = 1; i <= 6; i++) {
-		_area = document.getElementsByClassName("area" + i);
-		for (j = 0; j < _area.length; j++) {
-			_area[j].addEventListener('mouseover', warning);
-			_area[j].addEventListener('mouseout', myReset);
-		}
-		if (i <= 3) {
-			_path = document.getElementsByClassName("path" + i);
-			for (j = 0; j < _path.length; j++) _path[j].addEventListener('mouseover', changeMouse);
-		}
+function wallRecover(event) {
+	if (event.target.id == 'error') {
+		event.target.id = 'wall';
 	}
 }
 
 function changeMouse(event) {
-	if (start == 1) event.target.style.cursor = "pointer";
+	if (isStart) {
+		event.target.style.cursor = "pointer";
+	}
+}
+
+function addListener() {
+	let areaElement, pathElement;
+	for (let i = 1; i <= wallNumber; i++) {
+		areaElement = document.getElementsByClassName("area" + i);
+		for (let j = 0; j < areaElement.length; j++) {
+			areaElement[j].addEventListener('mouseover', warning);
+			areaElement[j].addEventListener('mouseout', wallRecover);
+		}
+		if (i <= pathNumber) {
+			pathElement = document.getElementsByClassName("path" + i);
+			for (let j = 0; j < pathElement.length; j++) {
+				pathElement[j].addEventListener('mouseover', changeMouse);
+			}
+		}
+	}
 }
 
 window.onload = function () {
 	addListener();
 	document.getElementById("s").onmouseover = function () {
-		if (start == 0) {
-			start = 1;
+		if (!isStart) {
+			isStart = true;
 			document.getElementById("result").textContent = 'Gaming';
 		}
 	}
 	document.getElementById("e").onmouseover = function () {
-		if (start == 1 && through == 1) {
-			start = 0;
-			through = 0;
+		if (isStart && isThrough) {
+			isStart = false;
+			isThrough = false;
 			document.getElementById("result").textContent = 'You Win';
-		} else if (start == 1 && through == 0) {
-			start = 0;
-			through = 0;
+		} else if (isStart && !isThrough) {
+			isStart = false;
+			isThrough = false;
 			document.getElementById("result").textContent = 'Don\'t cheat!';
 		}
 	}
 	document.getElementsByClassName("path3")[0].onmouseover = function () {
-		if (start == 1) {
-			if (through == 0) through = 1;
+		if (isStart && !isThrough) {
+			isThrough = true;
 		}
 	}
 }
