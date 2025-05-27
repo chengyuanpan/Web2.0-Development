043dc84161d6993797f9d22e8079adf2db1a2d88
Pan Chengyuan <panchy7@mail2.sysu.edu.cn>
2025-05-27 18:14:24 +0800

    Change code position

--- Assignment 7/table-sorter/sorter.js
+++ Assignment 7/table-sorter/sorter.js
@@ -1,5 +1,31 @@
 let order = { todo: 0, staff: 0 };
 
+// Swaps the content of two rows in a table
+const swap = (rowA, rowB) => {
+  let tmp;
+  tmp = rowA.innerHTML;
+  rowA.innerHTML = rowB.innerHTML;
+  rowB.innerHTML = tmp;
+};
+
+// Sorts the rows of a table based on the clicked header
+const sort = (tableId, i, rows) => {
+  let up;
+  for (let j = 1; j < 3; j++) {
+    up = rows[j];
+    for (let k = j + 1; k < 4; k++) {
+      if (order[tableId] == 0 || order[tableId] == -1) {
+        if (rows[k].cells[i].innerHTML < up.cells[i].innerHTML) up = rows[k];
+      } else {
+        if (rows[k].cells[i].innerHTML > up.cells[i].innerHTML) up = rows[k];
+      }
+    }
+    swap(rows[j], up);
+  }
+};
+
+// Determines which table to sort based on the clicked header
+// and calls the sort function
 const judge = (event) => {
   let tableId;
   if (
@@ -22,28 +48,6 @@ const judge = (event) => {
   $("#" + tableId + " th").attr("class", "normal");
   if (order[tableId] == 1) event.target.className = "sortedAscend";
   else event.target.className = "sortedDescend";
-};
-
-const sort = (tableId, i, rows) => {
-  let up;
-  for (let j = 1; j < 3; j++) {
-    up = rows[j];
-    for (let k = j + 1; k < 4; k++) {
-      if (order[tableId] == 0 || order[tableId] == -1) {
-        if (rows[k].cells[i].innerHTML < up.cells[i].innerHTML) up = rows[k];
-      } else {
-        if (rows[k].cells[i].innerHTML > up.cells[i].innerHTML) up = rows[k];
-      }
-    }
-    swap(rows[j], up);
-  }
-};
-
-const swap = (rowA, rowB) => {
-  let tmp;
-  tmp = rowA.innerHTML;
-  rowA.innerHTML = rowB.innerHTML;
-  rowB.innerHTML = tmp;
 };
 
 window.onload = () => {
