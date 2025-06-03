db0e532259607ad8b2da597fbd23ed6ac61d0f66
Pan Chengyuan <panchy7@mail2.sysu.edu.cn>
2025-06-03 11:28:40 +0800

    Format code style

--- Assignment 8/signin.js
+++ Assignment 8/signin.js
@@ -1,14 +1,14 @@
-var http=require('http');
-var fs=require('fs');
-var url=require('url');
-var queryString=require('querystring');
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var queryString = require('querystring');
 var database = new Array();
 
 function giveBack(response, filePath) {
-	fs.readFile(filePath, function(err, data) {
-		console.log("load file "+filePath);
-		var suffix = filePath.substr(filePath.lastIndexOf('.')+1, filePath.length);
-		response.writeHead(200, {"Content-Type":"text/"+suffix});
+	fs.readFile(filePath, function (err, data) {
+		console.log("load file " + filePath);
+		var suffix = filePath.substr(filePath.lastIndexOf('.') + 1, filePath.length);
+		response.writeHead(200, { "Content-Type": "text/" + suffix });
 		response.write(data.toString());
 		response.end();
 	});
@@ -22,10 +22,10 @@ function isValid(params) {
 }
 
 function print(response, info) {
-	response.write("Name:"+info['name']);
-	response.write("\nStudent ID:"+info["id"]);
-	response.write("\nEmail:"+info["email"]);
-	response.write("\nPhone:"+info["phone"]);
+	response.write("Name:" + info['name']);
+	response.write("\nStudent ID:" + info["id"]);
+	response.write("\nEmail:" + info["email"]);
+	response.write("\nPhone:" + info["phone"]);
 	response.end();
 }
 
@@ -40,32 +40,32 @@ function handleRegist(request, response, postData, database) {
 	}
 	if (isFound == 0) {
 		if (isValid(params)) {
-			var message = params['name']+","+params["id"]+","+params["email"]+","+params["phone"]+"\r\n";
+			var message = params['name'] + "," + params["id"] + "," + params["email"] + "," + params["phone"] + "\r\n";
 			database[database.length] = new Array();
-			database[database.length-1]['name'] = params['name'];
-			database[database.length-1]['id'] = params['id'];
-			database[database.length-1]['email'] = params['email'];
-			database[database.length-1]['phone'] = params['phone'];
-			fs.appendFile("database.txt", message, function(err, data) {});
-			response.writeHead(200, {"Content-Type":"text/plain;charset=utf-8"});
+			database[database.length - 1]['name'] = params['name'];
+			database[database.length - 1]['id'] = params['id'];
+			database[database.length - 1]['email'] = params['email'];
+			database[database.length - 1]['phone'] = params['phone'];
+			fs.appendFile("database.txt", message, function (err, data) { });
+			response.writeHead(200, { "Content-Type": "text/plain;charset=utf-8" });
 			print(response, params);
 		} else {
-			response.writeHead(200, {"Content-Type":"text/plain;charset=utf-8"});
+			response.writeHead(200, { "Content-Type": "text/plain;charset=utf-8" });
 			response.write("Your input information is incorrect");
 			response.end();
 		}
 	} else {
-		response.writeHead(200, {"Content-Type":"text/plain;charset=utf-8"});
+		response.writeHead(200, { "Content-Type": "text/plain;charset=utf-8" });
 		response.write("User already exists, information as follows:\n");
 		print(response, database[i]);
-	}	
+	}
 }
 
 function start() {
 	var database = new Array();
 	fs.readFile("database.txt", 'utf-8', function (err, data) {
 		var tmp1 = data.toString().split('\r\n');
-		for (var i = 0; i < tmp1.length-1; i++) {
+		for (var i = 0; i < tmp1.length - 1; i++) {
 			tmp2 = tmp1[i].split(",");
 			database[i] = {};
 			database[i]['name'] = tmp2[0];
@@ -87,21 +87,21 @@ function start() {
 			postData = "";
 		});
 		if ((pathname != "/logIn") && (search == null)) {
-			var suffix = pathname.substr(pathname.lastIndexOf('.')+1, pathname.length);
+			var suffix = pathname.substr(pathname.lastIndexOf('.') + 1, pathname.length);
 			if (suffix != 'css' && suffix != 'js') suffix = 'html';
-			giveBack(response, "register."+suffix);
-		} else if (search != null){
+			giveBack(response, "register." + suffix);
+		} else if (search != null) {
 			var isFound = 0;
 			for (var i = 0; i < database.length; i++) {
 				if (database[i]['name'] == query['username']) {
 					isFound = 1;
-					response.writeHead(200, {"Content-Type":"text/plain;charset=utf-8"});
+					response.writeHead(200, { "Content-Type": "text/plain;charset=utf-8" });
 					print(response, database[i]);
 					break;
 				}
 			}
 			if (isFound == 0) {
-				response.writeHead(200, {"Content-Type":"text/plain;charset=utf-8"});
+				response.writeHead(200, { "Content-Type": "text/plain;charset=utf-8" });
 				response.write("User does not exist");
 				response.end();
 			}
