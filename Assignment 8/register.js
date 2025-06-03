238e03cfb35d3474d4701d21fc050b7bc7c7d6e5
Pan Chengyuan <panchy7@mail2.sysu.edu.cn>
2025-06-03 11:29:56 +0800

    Format code style

--- Assignment 8/register.js
+++ Assignment 8/register.js
@@ -1,8 +1,8 @@
 window.onload = function () {
-	document.getElementById("reset").onclick = function () {
-		document.getElementById("name").value = "";
-		document.getElementById("id").value = "";
-		document.getElementById("email").value = "";
-		document.getElementById("phone").value = "";
-	}
-}
+  document.getElementById("reset").onclick = function () {
+    document.getElementById("name").value = "";
+    document.getElementById("id").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("phone").value = "";
+  };
+};
