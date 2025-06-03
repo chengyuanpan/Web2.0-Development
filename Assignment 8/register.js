a5a464edbf6e9026b8ed8de98a6fad650f3f4657
Pan Chengyuan <panchy7@mail2.sysu.edu.cn>
2025-06-03 18:36:38 +0800

    Upload Assignment 8

--- Assignment 8/register.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-  document.getElementById("reset").onclick = function () {
-    document.getElementById("name").value = "";
-    document.getElementById("id").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("phone").value = "";
-  };
-};
