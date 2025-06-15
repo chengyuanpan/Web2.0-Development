128d3b571b2aff269d07fdfca7e168584b7c0fdc
Pan Chengyuan <panchy7@mail2.sysu.edu.cn>
2025-06-15 23:04:54 +0800

    Improve log-print

--- Assignment 8/signServer.js
+++ Assignment 8/signServer.js
@@ -19,87 +19,90 @@ function find(Obj, callback) {
 }
 
 http.createServer(async (request, response) => {
-    const myURL = new url.URL(request.url, `http://${request.headers.host}`);
-    const content = Object.fromEntries(myURL.searchParams.entries());
-    console.log(content);
-    if (request.url == "/") {
-      // Home page
-      fs.readFile("./signup.html", "utf-8", (err, html) => {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.end(html);
-      });
-    } else if (request.url.startsWith("/signSearch")) {
-      // Check if content exists
-      find(content, (result) => {
-        response.end(`${!(result === false)}`);
-      });
-      // console.log(find(content));
-    } else if (request.url.startsWith("/?userName")) {
-      // User details
-      find(content, (result) => {
-        // console.log(result);
-        if (result === false) {
-          // User does not exist, return to registration
-          fs.readFile("./signup.html", "utf-8", (err, html) => {
-            response.writeHead(302, { location: "http://localhost:8000" });
-            response.end();
-          });
-        } else {
-          // User exists
-          fs.readFile("./user.html", "utf-8", (err, html) => {
-            html = html.replace("Target Username", result.userName);
-            html = html.replace("Target Student ID", result.studentID);
-            html = html.replace("Target Phone Number", result.phone);
-            html = html.replace("Target Email", result.email);
-
-            response.writeHead(200, { "Content-Type": "text/html" });
-            response.end(html);
-          });
-        }
-      });
-    } else if (request.url == "/signUpPost") {
-      // User registration
-      request.on("data", (chunk) => {
-        // console.log(chunk);
-        chunk = queryString.parse(chunk.toString());
-        find(chunk, (result) => {
-          if (!result) {
-            let user = {
-              userName: chunk.userName,
-              studentID: chunk.studentID,
-              phone: chunk.phone,
-              email: chunk.email,
-            };
-            if (user) {
-              fs.readFile(dataPath, "utf-8", (err, data) => {
-                let userData = JSON.parse(data);
-                userData.push(user);
-                fs.writeFile(dataPath, JSON.stringify(userData), (err) => {
-                  response.writeHead(302, {
-                    location: `http://localhost:8000/?userName=${user.userName}`,
-                  });
-                  response.end();
-                });
-              });
-            }
-          }
-        });
-      });
-    } else {
-      try {
-        // Load other files
-        fs.readFile(`.${request.url}`, (err, data) => {
-          let type = request.url.split(".")[1];
-          if (type == "png" || type == "jpg") type = `image/${type}`;
-          else type = `text/${type}`;
-          response.writeHead(200, { "Content-Type": type });
-          response.end(data);
-        });
-      } catch (err) {
+  const myURL = new url.URL(request.url, `http://${request.headers.host}`);
+  const content = Object.fromEntries(myURL.searchParams.entries());
+  if (Object.keys(content).length > 0) {
+    console.log('Received parameters:', content);
+  }
+  console.log(request.url);
+  if (request.url == "/") {
+    // Home page
+    fs.readFile("./signup.html", "utf-8", (err, html) => {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.end(html);
+    });
+  } else if (request.url.startsWith("/signSearch")) {
+    // Check if content exists
+    find(content, (result) => {
+      response.end(`${!(result === false)}`);
+    });
+    // console.log(find(content));
+  } else if (request.url.startsWith("/?userName")) {
+    // User details
+    find(content, (result) => {
+      // console.log(result);
+      if (result === false) {
+        // User does not exist, return to registration
         fs.readFile("./signup.html", "utf-8", (err, html) => {
+          response.writeHead(302, { location: "http://localhost:8000" });
+          response.end();
+        });
+      } else {
+        // User exists
+        fs.readFile("./user.html", "utf-8", (err, html) => {
+          html = html.replace("Target Username", result.userName);
+          html = html.replace("Target Student ID", result.studentID);
+          html = html.replace("Target Phone Number", result.phone);
+          html = html.replace("Target Email", result.email);
+
           response.writeHead(200, { "Content-Type": "text/html" });
           response.end(html);
         });
       }
+    });
+  } else if (request.url == "/signUpPost") {
+    // User registration
+    request.on("data", (chunk) => {
+      // console.log(chunk);
+      chunk = queryString.parse(chunk.toString());
+      find(chunk, (result) => {
+        if (!result) {
+          let user = {
+            userName: chunk.userName,
+            studentID: chunk.studentID,
+            phone: chunk.phone,
+            email: chunk.email,
+          };
+          if (user) {
+            fs.readFile(dataPath, "utf-8", (err, data) => {
+              let userData = JSON.parse(data);
+              userData.push(user);
+              fs.writeFile(dataPath, JSON.stringify(userData), (err) => {
+                response.writeHead(302, {
+                  location: `http://localhost:8000/?userName=${user.userName}`,
+                });
+                response.end();
+              });
+            });
+          }
+        }
+      });
+    });
+  } else {
+    try {
+      // Load other files
+      fs.readFile(`.${request.url}`, (err, data) => {
+        let type = request.url.split(".")[1];
+        if (type == "png" || type == "jpg") type = `image/${type}`;
+        else type = `text/${type}`;
+        response.writeHead(200, { "Content-Type": type });
+        response.end(data);
+      });
+    } catch (err) {
+      fs.readFile("./signup.html", "utf-8", (err, html) => {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.end(html);
+      });
     }
-  }).listen(8000);
+  }
+}).listen(8000);
