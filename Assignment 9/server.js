d5ad8db589df33e004682d6ba3ee07e01a190be1
Pan Chengyuan <panchy7@mail2.sysu.edu.cn>
2025-06-23 05:45:58 +0800

    Code style

--- Assignment 9/server.js
+++ Assignment 9/server.js
@@ -1,29 +1,17 @@
-var http = require('http');
-var url = require('url');
-var path = require('path');
-var fs = require('fs');
+var http = require("http");
+var url = require("url");
+var path = require("path");
+var fs = require("fs");
 var port = 3000;
-
-http.createServer(function(req,res){
-  var pathname = url.parse(req.url).pathname;
-  var mimeType = getMimeType(pathname);
-  if (!!mimeType) {
-    handlePage(req, res, pathname);
-  } else {
-    handleAjax(req, res);
-  }
-}).listen(port, function(){
-  console.log('server listen on ', port);
-});
 
 function getMimeType(pathname) {
   var validExtensions = {
-    ".html" : "text/html",
+    ".html": "text/html",
     ".js": "application/javascript",
     ".css": "text/css",
     ".jpg": "image/jpeg",
     ".gif": "image/gif",
-    ".png": "image/png"
+    ".png": "image/png",
   };
   var ext = path.extname(pathname);
   var type = validExtensions[ext];
@@ -34,7 +22,7 @@ function handlePage(req, res, pathname) {
   var filePath = __dirname + pathname;
   var mimeType = getMimeType(pathname);
   if (fs.existsSync(filePath)) {
-    fs.readFile(filePath, function(err, data){
+    fs.readFile(filePath, function (err, data) {
       if (err) {
         res.writeHead(500);
         res.end();
@@ -53,12 +41,26 @@ function handlePage(req, res, pathname) {
 
 function handleAjax(req, res) {
   var random_time = 1000 + getRandomNumber(2000);
-  var random_num  = 1 + getRandomNumber(9);
-  setTimeout(function(){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+  var random_num = 1 + getRandomNumber(9);
+  setTimeout(function () {
+    res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("" + random_num);
   }, random_time);
 }
+
+http
+  .createServer(function (req, res) {
+    var pathname = url.parse(req.url).pathname;
+    var mimeType = getMimeType(pathname);
+    if (!!mimeType) {
+      handlePage(req, res, pathname);
+    } else {
+      handleAjax(req, res);
+    }
+  })
+  .listen(port, function () {
+    console.log("server listen on ", port);
+  });
 
 function getRandomNumber(limit) {
   return Math.round(Math.random() * limit);
