743a6c3fc826a0e624bb096afe192b94800bd1e3
Pan Chengyuan <panchy7@mail2.sysu.edu.cn>
2025-09-08 17:14:58 +0800

    Improve

--- Assignment 9/S1/server.js
+++ Assignment 9/S1/server.js
@@ -55,7 +55,7 @@ function handleAjax(req, res) {
   // After a delay of random_time milliseconds, return a response
   setTimeout(function () {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("random_num: " + random_num);
+    res.end("handleAjax, random_num: " + random_num);
   }, random_time);
 }
 
