19ac53aec2f10a65fc68b10243395833fd40e840
Pan Chengyuan <panchy7@mail2.sysu.edu.cn>
2025-09-16 02:19:21 +0800

    Improve code

--- Assignment 9/S1/index.js
+++ Assignment 9/S1/index.js
@@ -3,6 +3,8 @@ window.onload = function () {
   let numberFetched = [false, false, false, false, false];
   const $buttons = $("#ring-container .button");
   const $sum = $("#sum");
+  const DECIMAL = 10;
+  const INFO_BAR = 5;
 
   function Reset() {
     // This selector will match all <span> elements on the page, clearing their contents.
@@ -19,7 +21,7 @@ window.onload = function () {
     );
     $("#info-bar").css("background-color", "#808080");
     buttonClickable = [true, true, true, true, true, false];
-    numberFetched = [false, false, false, false, false];
+    numberFetched.fill(false);
   }
 
   $("#button").mouseleave(Reset);
@@ -38,14 +40,14 @@ window.onload = function () {
     $(content).addClass("redSpot");
     $(content).text("...");
     $("#ring-container .button").css("background-color", "#707070");
-    buttonClickable = [false, false, false, false, false, false];
+    buttonClickable.fill(false);
     let index = getIndex(tar);
     $buttons[index].style.backgroundColor = "rgba(255, 123, 53, 1)";
     $.get("http://localhost:3000", function (res, status, XHR) {
       $(content).text(res);
       numberFetched[index] = true;
       let fetchedNumCounter = 0;
-      for (let i = 0; i < 5; i++) {
+      for (let i = 0; i < INFO_BAR; i++) {
         if (!numberFetched[i]) {
           buttonClickable[i] = true;
           $buttons[i].style.backgroundColor = "rgba(48, 159, 48, 1)";
@@ -55,30 +57,28 @@ window.onload = function () {
           $buttons[i].style.backgroundColor = "#707070";
         }
       }
-      if (fetchedNumCounter >= 5) {
-        buttonClickable[5] = true;
+      if (fetchedNumCounter >= INFO_BAR) {
+        buttonClickable[INFO_BAR] = true;
         $("#info-bar").css("background-color", "rgba(48, 159, 48, 1)");
       }
     });
   }
 
-  $("#ring-container .button").click(function (event) {
-    if (clickable(event.target)) {
-      action(event.target);
-    }
+  $("#ring-container .button").click(function () {
+    if(clickable(this)) action(this);
   });
 
   function getSum() {
-    if (buttonClickable[5]) {
+    if (buttonClickable[INFO_BAR]) {
       let sum = 0;
-      sum += parseInt($("#A span").html());
-      sum += parseInt($("#B span").html());
-      sum += parseInt($("#C span").html());
-      sum += parseInt($("#D span").html());
-      sum += parseInt($("#E span").html());
+      sum += parseInt($("#A span").html(), DECIMAL);
+      sum += parseInt($("#B span").html(), DECIMAL);
+      sum += parseInt($("#C span").html(), DECIMAL);
+      sum += parseInt($("#D span").html(), DECIMAL);
+      sum += parseInt($("#E span").html(), DECIMAL);
       $sum.html("" + sum);
       $("#info-bar").css("background-color", "rgba(48, 159, 48, 1)");
-      buttonClickable[5] = false;
+      buttonClickable[INFO_BAR] = false;
     }
   }
 
