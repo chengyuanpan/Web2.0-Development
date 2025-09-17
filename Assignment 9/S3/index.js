3e3a7561c721b15899e36870ea00cd6aef25227f
Pan Chengyuan <alvin.chengyuan.pan@gmail.com>
2025-09-17 16:33:06 +0800

    Rename veriables and fix magic number

--- Assignment 9/S3/index.js
+++ Assignment 9/S3/index.js
@@ -1,10 +1,17 @@
 window.onload = function () {
-  let clickbutton = [true, true, true, true, true, false, true];
-  let Number = [false, false, false, false, false];
+  let buttonClickable = [true, true, true, true, true, false, true];
+  let fetchedNumber = [false, false, false, false, false];
+  const INFO_BAR = 5;
+  const AT_BUTTON = 6;
+  // Writing 10 ensures that it is parsed as a decimal integer to avoid compatibility issues.
+  // If you don't specify the base, some browsers will automatically identify the base based on the string prefix (such as "0x", "0"), which may result in strange results.
+  const DECIMAL = 10;
+  const $buttons = $("#ring-container .button");
+  const $sum = $("#sum");
 
-  $("#button").mouseleave(Reset);
+  $("#button").mouseleave(reset);
 
-  $("#ring-container .button").click(function (event) {
+  $buttons.click(function (event) {
     if (Click(event.target)) {
       action(event.target);
     }
@@ -13,74 +20,74 @@ window.onload = function () {
   $("#info-bar").click(getsum);
 
   $(".apb").click(function (event) {
-    if (clickbutton[6]) {
-      clickbutton[6] = false;
+    if (buttonClickable[AT_BUTTON]) {
+      buttonClickable[AT_BUTTON] = false;
       seleteall();
     }
   });
 
-  function Reset() {
+  function reset() {
     $("span").html("");
     $(".text").removeClass("redSpot");
-    $("#ring-container .button").css(
+    $buttons.css(
       "background-color",
       "rgba(48, 63, 159, 1)"
     );
     $("#info-bar").css("background-color", "#707070");
-    clickbutton = [true, true, true, true, true, false, true];
-    Number = [false, false, false, false, false];
+    buttonClickable = [true, true, true, true, true, false, true];
+    fetchedNumber = [false, false, false, false, false];
   }
 
   function getsum() {
-    if (clickbutton[5]) {
+    if (buttonClickable[INFO_BAR]) {
       let sum = 0;
-      sum += parseInt($("#A span").html());
-      sum += parseInt($("#B span").html());
-      sum += parseInt($("#C span").html());
-      sum += parseInt($("#D span").html());
-      sum += parseInt($("#E span").html());
-      $("#sum").html(sum + "");
+      sum += parseInt($("#A span").html(), DECIMAL);
+      sum += parseInt($("#B span").html(), DECIMAL);
+      sum += parseInt($("#C span").html(), DECIMAL);
+      sum += parseInt($("#D span").html(), DECIMAL);
+      sum += parseInt($("#E span").html(), DECIMAL);
+      $sum.html("" + sum);
       $("#info-bar").css("background-color", "#707070");
-      clickbutton[5] = false;
+      buttonClickable[INFO_BAR] = false;
     }
   }
 
   function Click(tar) {
     let index = tar.id.charCodeAt() - "A".charCodeAt();
-    return clickbutton[index] && !Number[index];
+    return buttonClickable[index] && !fetchedNumber[index];
   }
 
   function action(tar) {
     let content = $(tar).find("span");
     $(content).addClass("redSpot");
     $(content).text("...");
-    $("#ring-container .button").css("background-color", "#707070");
-    clickbutton = [false, false, false, false, false, false];
+    $buttons.css("background-color", "#707070");
+    buttonClickable = [false, false, false, false, false, false];
     let index = tar.id.charCodeAt() - "A".charCodeAt();
-    Number[index] = true;
+    fetchedNumber[index] = true;
     $(".button")[index].style.backgroundColor = "rgba(48, 63, 159, 1)";
     $.get("http://localhost:3000", function (res, status, XHR) {
       $(content).text(res);
       let allnum = 0;
-      for (let i = 0; i < 5; i++) {
-        if (!Number[i]) {
-          clickbutton[i] = true;
+      for (let i = 0; i < INFO_BAR; i++) {
+        if (!fetchedNumber[i]) {
+          buttonClickable[i] = true;
           $(".button")[i].style.backgroundColor = "rgba(48, 63, 159, 1)";
         } else {
           allnum++;
-          clickbutton[i] = false;
+          buttonClickable[i] = false;
           $(".button")[i].style.backgroundColor = "#707070";
         }
       }
-      if (allnum >= 5) {
-        clickbutton[5] = true;
+      if (allnum >= INFO_BAR) {
+        buttonClickable[INFO_BAR] = true;
         $("#info-bar").css("background-color", "rgba(48, 63, 159, 1)");
       }
     });
   }
 
   function allNumber() {
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < INFO_BAR; i++) {
       if (
         $(".text:eq(" + i + ")").html() == "" ||
         $(".text:eq(" + i + ")").html() == "..."
@@ -92,20 +99,20 @@ window.onload = function () {
 
   function Callback() {
     let callback = [];
-    clickbutton = [false, false, false, false, false, false, true];
-    for (let index = 0; index < 5; index++) {
+    buttonClickable = [false, false, false, false, false, false, true];
+    for (let index = 0; index < INFO_BAR; index++) {
       (function (index) {
         callback[index] = function () {
           let content = $(".button:eq(" + index + ")").find("span");
           $(content).addClass("redSpot");
           $(content).text("...");
-          Number[index] = true;
+          fetchedNumber[index] = true;
           $(".button")[index].style.backgroundColor = "rgba(48, 63, 159, 1)";
           $.get("http://localhost:3000", function (data) {
             $(".text:eq(" + index + ")").text(data);
             $(".button")[index].style.backgroundColor = "#707070";
             if (allNumber()) {
-              clickbutton[5] = true;
+              buttonClickable[INFO_BAR] = true;
               $("#info-bar").css("background-color", "rgba(48, 63, 159, 1)");
               getsum();
             }
