5112befb60ee05306999307e3aebe811adb803a0
Pan Chengyuan <panchy7@mail2.sysu.edu.cn>
2025-09-17 09:36:45 +0800

    Improve code quality: change name and use let instead of var

--- Assignment 9/S2/index.js
+++ Assignment 9/S2/index.js
@@ -1,6 +1,6 @@
 window.onload = function () {
-  var clickbutton = [true, true, true, true, true, false, true];
-  var Number = [false, false, false, false, false];
+  let buttonClickable = [true, true, true, true, true, false, true];
+  let fetchedNumber = [false, false, false, false, false];
 
   $("#button").mouseleave(Reset);
 
@@ -13,8 +13,8 @@ window.onload = function () {
   $("#info-bar").click(getsum);
 
   $(".apb").click(function (event) {
-    if (clickbutton[6]) {
-      clickbutton[6] = false;
+    if (buttonClickable[6]) {
+      buttonClickable[6] = false;
       inorder();
     }
   });
@@ -27,13 +27,13 @@ window.onload = function () {
       "rgba(48, 63, 159, 1)"
     );
     $("#info-bar").css("background-color", "#707070");
-    clickbutton = [true, true, true, true, true, false, true];
-    Number = [false, false, false, false, false];
+    buttonClickable = [true, true, true, true, true, false, true];
+    fetchedNumber = [false, false, false, false, false];
   }
 
   function getsum() {
-    if (clickbutton[5]) {
-      var sum = 0;
+    if (buttonClickable[5]) {
+      let sum = 0;
       sum += parseInt($("#A span").html());
       sum += parseInt($("#B span").html());
       sum += parseInt($("#C span").html());
@@ -41,73 +41,73 @@ window.onload = function () {
       sum += parseInt($("#E span").html());
       $("#sum").html(sum + "");
       $("#info-bar").css("background-color", "#707070");
-      clickbutton[5] = false;
+      buttonClickable[5] = false;
     }
   }
 
   function Click(tar) {
-    var index = tar.id.charCodeAt() - "A".charCodeAt();
-    return clickbutton[index] && !Number[index];
+    let index = tar.id.charCodeAt() - "A".charCodeAt();
+    return buttonClickable[index] && !fetchedNumber[index];
   }
 
   function action(tar) {
-    var content = $(tar).find("span");
+    let content = $(tar).find("span");
     $(content).addClass("redSpot");
     $(content).text("...");
     $("#ring-container .button").css("background-color", "#707070");
-    clickbutton = [false, false, false, false, false, false];
-    var index = tar.id.charCodeAt() - "A".charCodeAt();
-    Number[index] = true;
+    buttonClickable = [false, false, false, false, false, false];
+    let index = tar.id.charCodeAt() - "A".charCodeAt();
+    fetchedNumber[index] = true;
     $(".button")[index].style.backgroundColor = "rgba(48, 63, 159, 1)";
     $.get("http://localhost:3000", function (res, status, XHR) {
       $(content).text(res);
-      var allnum = 0;
-      for (var i = 0; i < 5; i++) {
-        if (!Number[i]) {
-          clickbutton[i] = true;
+      let allnum = 0;
+      for (let i = 0; i < 5; i++) {
+        if (!fetchedNumber[i]) {
+          buttonClickable[i] = true;
           $(".button")[i].style.backgroundColor = "rgba(48, 63, 159, 1)";
         } else {
           allnum++;
-          clickbutton[i] = false;
+          buttonClickable[i] = false;
           $(".button")[i].style.backgroundColor = "#707070";
         }
       }
       if (allnum >= 5) {
-        clickbutton[5] = true;
+        buttonClickable[5] = true;
         $("#info-bar").css("background-color", "rgba(48, 63, 159, 1)");
       }
     });
   }
 
   function Callback(order) {
-    var callback = [];
-    var buttons = [];
-    for (var i = 0; i < order.length; i++) {
+    let callback = [];
+    let buttons = [];
+    for (let i = 0; i < order.length; i++) {
       buttons[i] = document.querySelector("#" + order[i]);
     }
-    for (var i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i++) {
       (function (i) {
-        var next = i + 1;
+        let next = i + 1;
         callback[i] = function () {
-          var tar = buttons[i];
-          var content = $(tar).find("span");
+          let tar = buttons[i];
+          let content = $(tar).find("span");
           $(content).addClass("redSpot");
           $(content).text("...");
           $("#ring-container .button").css("background-color", "#707070");
-          clickbutton = [false, false, false, false, false, false];
-          var index = tar.id.charCodeAt() - "A".charCodeAt();
-          Number[index] = true;
+          buttonClickable = [false, false, false, false, false, false];
+          let index = tar.id.charCodeAt() - "A".charCodeAt();
+          fetchedNumber[index] = true;
           $(".button")[index].style.backgroundColor = "rgba(48, 63, 159, 1)";
           $.get("http://localhost:3000", function (res, status, XHR) {
             $(content).text(res);
-            var allnum = 0;
-            for (var i = 0; i < 5; i++) {
-              if (!Number[i]) {
-                clickbutton[i] = true;
+            let allnum = 0;
+            for (let i = 0; i < 5; i++) {
+              if (!fetchedNumber[i]) {
+                buttonClickable[i] = true;
                 $(".button")[i].style.backgroundColor = "rgba(48, 63, 159, 1)";
               } else {
                 allnum++;
-                clickbutton[i] = false;
+                buttonClickable[i] = false;
                 $(".button")[i].style.backgroundColor = "#707070";
               }
             }
@@ -117,7 +117,7 @@ window.onload = function () {
       })(i);
     }
     callback[5] = function () {
-      clickbutton[5] = true;
+      buttonClickable[5] = true;
       $("#info-bar").css("background-color", "rgba(48, 63, 159, 1)");
       getsum();
     };
@@ -125,8 +125,8 @@ window.onload = function () {
   }
 
   function inorder() {
-    var order = ["A", "B", "C", "D", "E"];
-    var callback = Callback(order);
+    let order = ["A", "B", "C", "D", "E"];
+    let callback = Callback(order);
     callback[0]();
   }
 };
