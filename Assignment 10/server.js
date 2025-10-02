58882ed7cd6a3e7cb693cd68ac16574734c41380
Chengyuan Pan <alvin.chengyuan.pan@gmail.com>
2025-10-02 11:59:54 +0800

    Correct file path

--- Assignment 10/server.js
+++ Assignment 10/server.js
@@ -29,7 +29,7 @@ app.use( // Registers middleware in Express
 
 // Registration page
 app.get('/regist', (req, res) => {
-  fs.readFile('signup.html', 'utf-8', (err, data) => {
+  fs.readFile('./assets/html/signup.html', 'utf-8', (err, data) => {
     res.send(data);
   });
 });
@@ -71,7 +71,7 @@ app.post('/signUpPost', urlencodedParser, (req, res) => {
 
 // Login page
 app.get('/login', (req, res) => {
-  fs.readFile('login.html', 'utf-8', (err, data) => {
+  fs.readFile('./assets/html/login.html', 'utf-8', (err, data) => {
     res.send(data);
     return;
   });
@@ -88,14 +88,14 @@ app.post('/login', urlencodedParser, (req, res, next) => {
   db.find({ userName: tar.userName }, (result) => {
     // The username does not exist
     if (result.length == 0) {
-      res.render('./jump', { tarStr: 'UsernameDoesNotExist', tarAdd: '/login' });
+      res.render('jump', { tarStr: 'UsernameDoesNotExist', tarAdd: '/login' });
       // res.redirect('http://localhost:8000/jump?string=UsernameDoesNotExist');
       return;
     } else {
       db.find(tar, (result) => {
         // Error password
         if (result.length == 0) {
-          res.render('./jump', { tarStr: 'IncorrectPassword', tarAdd: '/login' });
+          res.render('jump', { tarStr: 'IncorrectPassword', tarAdd: '/login' });
           // res.redirect('http://localhost:8000/jump?string=IncorrectPassword');
           return;
         } else {
