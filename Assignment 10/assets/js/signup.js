409af111b229814858904da9608e9246a597f492
Alvin Pan <alvin.chengyuan.pan@gmail.com>
2025-10-10 01:51:10 +0800

    Code style

--- Assignment 10/assets/js/signup.js
+++ Assignment 10/assets/js/signup.js
@@ -1,11 +1,11 @@
 (function () {
-  if (typeof $ === 'undefined') {
+  if (typeof $ === "undefined") {
     let flag = 0,
-      a = document.createElement('script'),
-      b = document.createElement('script');
-    a.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.js';
+      a = document.createElement("script"),
+      b = document.createElement("script");
+    a.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.js";
     document.body.appendChild(a);
-    b.src = 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.1/lodash.js';
+    b.src = "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.1/lodash.js";
     document.body.appendChild(b);
     a.onload = b.onload = () => {
       flag++;
@@ -21,37 +21,37 @@
       email: /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/, //
       password: /[\w-]{6,12}/, //密码为6~12位数字、大小写字母、中划线、下划线
     };
-    $('.info').blur(function () {
-      let name = $(this).attr('name');
+    $(".info").blur(function () {
+      let name = $(this).attr("name");
       if ($(this).val().length == 0) {
-        status[$(this).attr('name')] = false;
-        $(`#${name}`).text(`请填写${$(this).attr('placeholder')}`);
-        $(`#${name}`).css('opacity', '1');
-        $(`#${name}`).css('color', `#ff5a6a`);
+        status[$(this).attr("name")] = false;
+        $(`#${name}`).text(`请填写${$(this).attr("placeholder")}`);
+        $(`#${name}`).css("opacity", "1");
+        $(`#${name}`).css("color", `#ff5a6a`);
         status[name] = false;
         return;
       } else {
         $.get(
-          'http://localhost:8000/signSearch',
+          "http://localhost:8000/signSearch",
           $(this).val().length == 0 ? {} : { [name]: $(this).val() },
           (data) => {
             if (!reg[name].test($(this).val())) {
-              $(`#${name}`).css('opacity', '1');
-              $(`#${name}`).css('color', `#ff5a6a`);
-              $(`#${name}`).text(`${$(this).attr('placeholder')}不符合规则`);
+              $(`#${name}`).css("opacity", "1");
+              $(`#${name}`).css("color", `#ff5a6a`);
+              $(`#${name}`).text(`${$(this).attr("placeholder")}不符合规则`);
               status[name] = false;
-            } else if (data == 'true') {
+            } else if (data == "true") {
               //用户名已经存在
-              $(`#${name}`).css('opacity', '1');
-              $(`#${name}`).css('color', `#ff5a6a`);
-              $(`#${name}`).text(`${$(this).attr('placeholder')}已经存在`);
+              $(`#${name}`).css("opacity", "1");
+              $(`#${name}`).css("color", `#ff5a6a`);
+              $(`#${name}`).text(`${$(this).attr("placeholder")}已经存在`);
               status[name] = false;
               // $("#name").css("opacity", "1");
             } else {
-              $(`#${name}`).css('opacity', '1');
-              $(`#${name}`).css('color', `#ff5a6a`);
-              $(`#${name}`).text(`该${$(this).attr('placeholder')}可以使用`);
-              $(`#${name}`).css('color', '#42ca6b');
+              $(`#${name}`).css("opacity", "1");
+              $(`#${name}`).css("color", `#ff5a6a`);
+              $(`#${name}`).text(`该${$(this).attr("placeholder")}可以使用`);
+              $(`#${name}`).css("color", "#42ca6b");
               status[name] = true;
               // $("#name").css("opacity", "1");
             }
@@ -60,55 +60,55 @@
       }
     });
 
-    $('.pwd').blur(function () {
-      let name = $(this).attr('name');
+    $(".pwd").blur(function () {
+      let name = $(this).attr("name");
       if ($(this).val().length == 0) {
-        status[$(this).attr('name')] = false;
-        $(`#${name}`).text(`请填写${$(this).attr('placeholder')}`);
-        $(`#${name}`).css('opacity', '1');
-        $(`#${name}`).css('color', `#ff5a6a`);
+        status[$(this).attr("name")] = false;
+        $(`#${name}`).text(`请填写${$(this).attr("placeholder")}`);
+        $(`#${name}`).css("opacity", "1");
+        $(`#${name}`).css("color", `#ff5a6a`);
         status[name] = false;
       } else if (!reg[name].test($(this).val())) {
-        $(`#${name}`).css('opacity', '1');
-        $(`#${name}`).css('color', `#ff5a6a`);
-        $(`#${name}`).text(`${$(this).attr('placeholder')}不符合规则`);
+        $(`#${name}`).css("opacity", "1");
+        $(`#${name}`).css("color", `#ff5a6a`);
+        $(`#${name}`).text(`${$(this).attr("placeholder")}不符合规则`);
         status[name] = false;
       } else {
-        $(`#${name}`).css('opacity', '1');
-        $(`#${name}`).css('color', `#ff5a6a`);
-        $(`#${name}`).text(`该${$(this).attr('placeholder')}可以使用`);
-        $(`#${name}`).css('color', '#42ca6b');
+        $(`#${name}`).css("opacity", "1");
+        $(`#${name}`).css("color", `#ff5a6a`);
+        $(`#${name}`).text(`该${$(this).attr("placeholder")}可以使用`);
+        $(`#${name}`).css("color", "#42ca6b");
         status[name] = true;
       }
     });
 
-    $('.pwdr').blur(function () {
-      let name = $(this).attr('name');
+    $(".pwdr").blur(function () {
+      let name = $(this).attr("name");
       if ($(this).val().length == 0) {
-        $(`#${name}`).css('opacity', '1');
-        $(`#${name}`).css('color', `#ff5a6a`);
-        $(`#${name}`).text(`${$(this).attr('placeholder')}为空`);
-      } else if ($(this).val() != $('.pwd').val()) {
-        $(`#${name}`).css('opacity', '1');
-        $(`#${name}`).css('color', `#ff5a6a`);
-        $(`#${name}`).text(`${$(this).attr('placeholder')}不一样`);
+        $(`#${name}`).css("opacity", "1");
+        $(`#${name}`).css("color", `#ff5a6a`);
+        $(`#${name}`).text(`${$(this).attr("placeholder")}为空`);
+      } else if ($(this).val() != $(".pwd").val()) {
+        $(`#${name}`).css("opacity", "1");
+        $(`#${name}`).css("color", `#ff5a6a`);
+        $(`#${name}`).text(`${$(this).attr("placeholder")}不一样`);
         status[name] = false;
       } else {
-        $(`#${name}`).css('opacity', '1');
-        $(`#${name}`).css('color', `#ff5a6a`);
-        $(`#${name}`).text(`${$(this).attr('placeholder')}一样`);
-        $(`#${name}`).css('color', '#42ca6b');
+        $(`#${name}`).css("opacity", "1");
+        $(`#${name}`).css("color", `#ff5a6a`);
+        $(`#${name}`).text(`${$(this).attr("placeholder")}一样`);
+        $(`#${name}`).css("color", "#42ca6b");
         status[name] = true;
       }
     });
 
-    $('#reset').click(function () {
-      $('span').css('opicaty', '0');
-      $('input[type=text]').val(``);
+    $("#reset").click(function () {
+      $("span").css("opicaty", "0");
+      $("input[type=text]").val(``);
     });
 
-    $('form').on(`submit`, function (event) {
-      $('input[type=text]').blur();
+    $("form").on(`submit`, function (event) {
+      $("input[type=text]").blur();
       if (
         !(
           status.userName &&
